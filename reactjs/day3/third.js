8ed25b18416e968bf7bd01873f77fc04f8cdb2ce
Sumit Pokhriyal <sumitpokhriyalonezero@gmail.com>
2025-07-06 02:56:23 -0700

    JSX X bundler

---
+++ reactjs/day3/third.js
@@ -0,0 +1,49 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const elememt = React.createElement("h1",{id:"first",className:"rahul",style:{
+    fontSize:"30px",
+    backgroundColor:"green",
+    color:"pink"
+}},"hello coder army");
+const elememt2 = React.createElement("h1",{id:"first",className:"rahul",style:{
+    fontSize:"30px",
+    backgroundColor:"green",
+    color:"pink"
+}},"Maja aya mujhe");
+
+
+
+//JSX://JSX:javascript jaisa code direct js ke andar likh sakte ho
+
+
+const name="sumit"
+const object={
+    name:"sumit",
+    age:23,
+    salary:60
+}
+const newelement=(
+    <>
+    <h1 id="first" className="sumit">hello coder army {name} his salary is :{object.salary}</h1>
+    <h1>sumit pokhriyal</h1>
+    </>
+
+) //html directly js me jsx ka part)
+//USE CREATEROOT FOR REACT18
+const reactvoots =ReactDOM.createRoot(document.getElementById('root'));//[REACT ROOT CONTAINER :ME ROOT RK DIYA]
+reactvoots.render(newelement);
+
+
+
+
+
+//const div1=React.createElement("div",{},[elememt,elememt2])
+//ReactDOM.render(elememt,document.getElementById('root'))  //DONT WRITE LIKE THIS IN REACT 18 AS IT IS DISCARDED IN R18 ONLY WORKS UPTO 17
+
+
+//reactvoots.render(elememt);
+//reactvoots.render(elememt2);
+
+
